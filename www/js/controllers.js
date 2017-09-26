16300eb43a47a3549acacdb06bee81d5885f7169
SageArt <cliffshawn109@gmail.com>
2017-09-26 19:41:06 +0300

    v1.0.0

---
+++ www/js/controllers.js
@@ -0,0 +1,59 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', ['$scope','$http', '$state', function($scope, $http,$state) {
+    $http.get('js/data.json').success(function(data) {
+        $scope.artists = data;
+        $scope.whichartist = $state.params.aId;
+        console.log(data);
+    })
+  
+}])
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('tabs', {
+        url: '/tab',
+        abstract:true,  
+        templateUrl:'templates/tabs.html'
+    })
+    .state('tabs.list',{
+        url: '/dash',
+        views: {
+            'list-tab' : {
+                templateUrl: 'templates/list.html',
+                controller:'ChatDetailCtrl'
+            }
+        }
+    })
+    .state('tabs.detail', {
+    url: '/dash/:aId',
+    views: {
+        'list-tab' :{
+            templateUrl: 'templates/detail.html',
+            controller: 'ChatDetailCtrl'
+        }
+    }
+})
+})
